0a6734944fce832c7bad0f86bfad6d55ab51018e
Administrator <Administrator@LX>
2019-08-15 14:00:23 +0800

    xiuga

--- src/com/cn/Checkname.java
+++ src/com/cn/Checkname.java
@@ -17,9 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Checkname extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String name=request.getParameter("name");
 		PrintWriter q=response.getWriter();
